695397a5519df4faa7aaa64bb56e8af81a4072d9
Shahin <you@example.com>
2026-01-02 14:53:08 +0530

    update

---
+++ memo/src/Child.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Child = React.memo(function Child({ user, sayHello }) {
+  console.log("Child rendered");
+
+  return (
+    <>
+      <p>{user.name}</p>
+      <button onClick={sayHello}>Say Hi</button>
+    </>
+  );
+});
+
+export default Child;
