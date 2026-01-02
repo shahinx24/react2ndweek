1d29878235a2976aa548efa0087e23d0708f0979
Shahin <you@example.com>
2026-01-02 14:51:56 +0530

    update

--- memo/src/App.jsx
+++ memo/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useMemo } from "react";
+import Child from "./Child";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Parent() {
+  const [count, setCount] = useState(0);
+
+  // ✅ memoized object
+  const user = useMemo(() => {
+    return { name: "Shahin" };
+  }, []);
+
+  // ✅ memoized function
+  const sayHello = useCallback(() => {
+    console.log("Hi");
+  }, []);
+
+  console.log("Parent rendered");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <Child user={user} sayHello={sayHello} />
     </>
-  )
+  );
 }
-
-export default App
