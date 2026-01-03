707fb79dc7c899d6b344fa9ed02eab8fe43f6861
Shahin <you@example.com>
2026-01-03 13:31:50 +0530

    update

--- blogs/src/pages/BlogDetails.jsx
+++ blogs/src/pages/BlogDetails.jsx
@@ -2,9 +2,7 @@ import { useParams } from "react-router-dom";
 
 export default function BlogDetails({ blogs }) {
   const { id } = useParams();
-
   const blog = blogs.find((b) => b.id === Number(id));
-
   if (!blog) return <h3>Blog not found</h3>;
 
   return (
--- blogs/src/pages/Blogs.jsx
+++ blogs/src/pages/Blogs.jsx
@@ -4,9 +4,7 @@ export default function Blogs({ blogs }) {
   return (
     <>
       <h2>All Blogs</h2>
-
       {blogs.length === 0 && <p>No blogs yet</p>}
-
       {blogs.map((blog) => (
         <div key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
