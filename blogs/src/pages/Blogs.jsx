110e926e6e727c3caca496c905865b833cfd0648
Shahin <you@example.com>
2026-01-03 13:44:33 +0530

    update

--- blogs/src/pages/Blogs.jsx
+++ blogs/src/pages/Blogs.jsx
@@ -1,18 +1,19 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { BlogContext } from "../context/BlogContext";
 
-export default function Blogs({ blogs }) {
+export default function Blogs() {
+  const { blogs } = useContext(BlogContext);
+
   return (
     <>
-      <h2>All Blogs</h2>
-
+      <h2>Blogs</h2>
       {blogs.length === 0 && <p>No blogs yet</p>}
-
       {blogs.map((blog) => (
         <div key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
             <h3>{blog.title}</h3>
           </Link>
-          <small>{blog.date} • {blog.time}</small>
         </div>
       ))}
     </>
