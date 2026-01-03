83df4a98485aedfdb448f07a3ab25f4b0fd9678c
Shahin <you@example.com>
2026-01-03 11:02:00 +0530

    update

--- blogs/src/main.jsx
+++ blogs/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter } form "react-router-dom"
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BroeserRouter>,
 )
