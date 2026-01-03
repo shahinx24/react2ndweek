b9c20c5f1339f9b26204d322314aa695d7c0e19b
Shahin <you@example.com>
2026-01-03 13:45:08 +0530

    update

--- blogs/src/pages/BlogDetails.jsx
+++ blogs/src/pages/BlogDetails.jsx
@@ -1,24 +1,20 @@
 import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { BlogContext } from "../context/BlogContext";
 
-export default function BlogDetails({ blogs }) {
+export default function BlogDetails() {
   const { id } = useParams();
-
+  const { blogs } = useContext(BlogContext);
   const blog = blogs.find((b) => b.id === Number(id));
-
   if (!blog) return <h3>Blog not found</h3>;
 
   return (
     <>
       <h2>{blog.title}</h2>
       <p>{blog.body}</p>
-
       <hr />
-
       <p>
-        <strong>Date:</strong> {blog.date}
-      </p>
-      <p>
-        <strong>Time:</strong> {blog.time}
+        <strong>Created at:</strong> {blog.createdAt}
       </p>
     </>
   );
