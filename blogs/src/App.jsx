401618eca6f9113e2f51d0fe4cffb0299bb3b69c
Shahin <you@example.com>
2026-01-03 13:43:25 +0530

    update

--- blogs/src/App.jsx
+++ blogs/src/App.jsx
@@ -1,24 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useState } from "react";
 import CreateBlog from "./pages/CreateBlog";
 import Blogs from "./pages/Blogs";
 import BlogDetails from "./pages/BlogDetails";
 
 export default function App() {
-  const [blogs, setBlogs] = useState([]);
-
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/createblog" />} />
-      <Route
-        path="/createblog"
-        element={<CreateBlog blogs={blogs} setBlogs={setBlogs} />}
-      />
-      <Route path="/blogs" element={<Blogs blogs={blogs} />} />
-      <Route
-        path="/blogs/:id"
-        element={<BlogDetails blogs={blogs} />}
-      />
+      <Route path="/createblog" element={<CreateBlog />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/blogs/:id" element={<BlogDetails />} />
     </Routes>
   );
 }
