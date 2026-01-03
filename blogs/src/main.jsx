fd95bae6cae462f3b6899bab9dd036cc76799f1e
Shahin <you@example.com>
2026-01-03 11:36:56 +0530

    update

--- blogs/src/main.jsx
+++ blogs/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import { BrowserRouter } form "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BroeserRouter>,
+  </BrowserRouter>,
 )
