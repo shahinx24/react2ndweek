87a4ff54b3f27ca90b322e2a32a2928d6459cf74
Shahin <you@example.com>
2026-01-05 16:00:23 +0530

    update

---
+++ todo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  if (action.type === "ADD") {
+    return [...state, action.text];
+  }
+  if (action.type === "DELETE") {
+    return state.filter((_, index) => index !== action.index);
+  }
+  return state;
+}
+
+export default function TodoApp() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [text, setText] = useState("");
+
+  function addTodo() {
+    dispatch({ type: "ADD", text });
+    setText("");
+  }
+
+  return (
+    <>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => dispatch({ type: "DELETE", index })}>
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
