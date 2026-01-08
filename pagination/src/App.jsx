de856e2d93b0fbcf97e56fc920a5d52f5abc3347
Shahin <shahinofficial911@gmail.com>
2026-01-08 10:56:51 +0530

    update

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -2,42 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
+  const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-  const [limit] = useState(5);
-  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:3000/users?_page=${page}&_limit=${limit}`)
-      .then((res) => {
-        setUsers(res.data);
-        const total = res.headers["x-total-count"];
-        setTotalPages(Math.ceil(total / limit));
-      });
+      .get(`http://localhost:3000/users?_page=${page}&_limit=2`)
+      .then(res => setData(res.data));
   }, [page]);
 
   return (
     <div>
-      <h2>Users List (Pagination)</h2>
-
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.id}. {user.name}
-          </li>
-        ))}
-      </ul>
-
-      <button disabled={page === 1} onClick={() => setPage(page - 1)}>
-        Prev
-      </button>
-
-      <span> Page {page} of {totalPages} </span>
-
-      <button disabled={page === totalPages} onClick={() => setPage(page + 1)}>
-        Next
-      </button>
+      <h3>Pagination</h3>
+      {data.map(u => (
+        <p key={u.id}>{u.id} - {u.name}</p>
+      ))}
+      <button disabled={page===1} onClick={() => setPage(page-1)}>Prev</button>
+      <button onClick={() => setPage(page+1)}>Next</button>
     </div>
   );
 }
