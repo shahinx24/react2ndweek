0a28cd5779894aa7caa30f032243e848b72f639e
Shahin <shahinofficial911@gmail.com>
2026-01-08 10:49:08 +0530

    update

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(5);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users?_page=${page}&_limit=${limit}`)
+      .then((res) => {
+        setUsers(res.data);
+        const total = res.headers["x-total-count"];
+        setTotalPages(Math.ceil(total / limit));
+      });
+  }, [page]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Users List (Pagination)</h2>
 
-export default App
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id}. {user.name}
+          </li>
+        ))}
+      </ul>
+
+      <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+        Prev
+      </button>
+
+      <span> Page {page} of {totalPages} </span>
+
+      <button disabled={page === totalPages} onClick={() => setPage(page + 1)}>
+        Next
+      </button>
+    </div>
+  );
+}
