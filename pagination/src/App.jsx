28df6bceee86f02c511c333177aff04cea01ca07
Shahin <shahinofficial911@gmail.com>
2026-01-08 11:03:06 +0530

    update

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -6,17 +6,15 @@ export default function App() {
   const [page, setPage] = useState(1);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3000/users?_page=${page}&_limit=2`)
-      .then(res => setData(res.data));
+    axios.get(`http://localhost:3001/users?_page=${page}&_limit=2`)
+      .then(res => setData(res.data))
+      .catch(err => console.log("Error:", err.message));
   }, [page]);
 
   return (
     <div>
       <h3>Pagination</h3>
-      {data.map(u => (
-        <p key={u.id}>{u.id} - {u.name}</p>
-      ))}
+      {data.map(u => <p key={u.id}>{u.id} - {u.name}</p>)}
       <button disabled={page===1} onClick={() => setPage(page-1)}>Prev</button>
       <button onClick={() => setPage(page+1)}>Next</button>
     </div>
